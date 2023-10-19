9def065633dd7a70b56bd223bc867bcef23f4add
sonal0409 <mittal.sonal04@gmail.com>
2023-10-19 08:51:27 +0530

    done

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"7617574f-621a-4ffd-a160-a91464305f36","feature":"Rediff Login page","scenario":"Enter details on the Registration Page","start":1697620636788,"group":1,"content":"","tags":"@all,@register,","end":1697620651459,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
